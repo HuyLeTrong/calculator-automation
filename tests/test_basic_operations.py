0a7cecbadfe907da10c0cbccc32f7c6abd77f63e
TestHLT <106044424+HuyLeTrong@users.noreply.github.com>
2026-05-05 17:00:14 +0700

    add additional test
    add test for subtraction, negative subtraction, multiplication

--- tests/test_basic_operations.py
+++ tests/test_basic_operations.py
@@ -20,11 +20,6 @@ class TestBasicOperations(CalculatorSetupTeardownTest):
         Display = self.wait.until(
             EC.presence_of_element_located((By.NAME, "Display is 2")))
 
-        # self.assertIsNotNone(Expression, "Expression element not found")
-        # self.assertEqual(Expression.text, "Expression is 1 + 1=", "Expression text does not match expected value")
-        # self.assertIsNotNone(Display, "Display element not found")
-        # self.assertEqual(Display.text, "Display is 2", "Display text does not match expected value")
-        
         self.assertEqual(
             Expression.get_attribute("Name"), 
             "Expression is 1 + 1=",
@@ -33,5 +28,79 @@ class TestBasicOperations(CalculatorSetupTeardownTest):
         self.assertEqual(
             Display.get_attribute("Name"),
             "Display is 2", 
+            "Display does not match expected value"
+        )
+
+    def test_subtraction(self):
+        self.wait.until(EC.element_to_be_clickable((By.NAME, "One"))).click()
+        self.wait.until(EC.element_to_be_clickable((By.NAME, "Minus"))).click()
+        self.wait.until(EC.element_to_be_clickable((By.NAME, "One"))).click()
+        self.wait.until(EC.element_to_be_clickable((By.NAME, "Equals"))).click()
+
+        Expression = self.wait.until(
+            EC.presence_of_element_located((By.NAME, "Expression is 1 Minus ( 1=")))
+
+        Display = self.wait.until(
+            EC.presence_of_element_located((By.NAME, "Display is 0")))
+
+        
+        self.assertEqual(
+            Expression.get_attribute("Name"), 
+            "Expression is 1 Minus ( 1=",
+            "Expression does not match expected value"
+        )
+        self.assertEqual(
+            Display.get_attribute("Name"),
+            "Display is 0", 
+            "Display does not match expected value"
+        )
+
+    def test_negative_subtraction(self):
+        self.wait.until(EC.element_to_be_clickable((By.NAME, "Two"))).click()
+        self.wait.until(EC.element_to_be_clickable((By.NAME, "Minus"))).click()
+        self.wait.until(EC.element_to_be_clickable((By.NAME, "One"))).click()
+        self.wait.until(EC.element_to_be_clickable((By.NAME, "Positive negative"))).click()
+
+        self.wait.until(EC.element_to_be_clickable((By.NAME, "Equals"))).click()
+
+        Expression = self.wait.until(
+            EC.presence_of_element_located((By.NAME, "Expression is 2 Minus ( -1=")))
+
+        Display = self.wait.until(
+            EC.presence_of_element_located((By.NAME, "Display is 3")))
+
+        
+        self.assertEqual(
+            Expression.get_attribute("Name"), 
+            "Expression is 2 Minus ( -1=",
+            "Expression does not match expected value"
+        )
+        self.assertEqual(
+            Display.get_attribute("Name"),
+            "Display is 3", 
+            "Display does not match expected value"
+        )
+
+    def test_multiplication(self):
+        self.wait.until(EC.element_to_be_clickable((By.NAME, "One"))).click()
+        self.wait.until(EC.element_to_be_clickable((By.NAME, "One"))).click()
+        self.wait.until(EC.element_to_be_clickable((By.NAME, "Multiply by"))).click()
+        self.wait.until(EC.element_to_be_clickable((By.NAME, "Three"))).click()
+        self.wait.until(EC.element_to_be_clickable((By.NAME, "Equals"))).click()
+
+        Expression = self.wait.until(
+            EC.presence_of_element_located((By.NAME, "Expression is 11 × 3=")))
+        Display = self.wait.until(
+            EC.presence_of_element_located((By.NAME, "Display is 33")))
+
+        
+        self.assertEqual(
+            Expression.get_attribute("Name"), 
+            "Expression is 11 × 3=",
+            "Expression does not match expected value"
+        )
+        self.assertEqual(
+            Display.get_attribute("Name"),
+            "Display is 33", 
             "Display does not match expected value"
         )
